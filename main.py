9f8ad21f1c57520e0d5ba0f3017739718c0f5964
atairbekov <atairbekov@cs.stonybrook.edu>
2016-08-12 01:26:00 -0400

    records are now being committed

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from flask import Flask, jsonify, request, send_from_directory
 from flask_sqlalchemy import *
 import praw
 import requests
-
+import signal
 import redis
+import random
 
 app = Flask(__name__)
 
@@ -17,6 +18,7 @@ db = SQLAlchemy(app)
 
 # Start redis instance with default parameters
 redis_instance = redis.StrictRedis(host='localhost', port=6379)
+
 
 class Record(db.Model):
     id = db.Column(db.Integer, db.Sequence('record_reg_id', start=1, increment=1), primary_key=True)
@@ -44,16 +46,15 @@ def commit_new_record(message):
         logging.debug('new record added!')
         db.session.commit()
         logging.debug('new record committed!')
-        ret = {'Status', 'success!'}
+        ret = {'Status': 'success!'}
     except:
         ret = {'Status': 'failure on input "%s"'.format(message['data'])} 
     redis_instance.publish('db-records-commit-'+username+':'+timecr, json.dumps(ret))
   
 
-db_subscriber = redis_instance.pubsub()
+db_subscriber = redis_instance.pubsub(ignore_subscribe_messages=True)
 db_subscriber.subscribe(**{'db-records-filter': filter_records_by, 'db-records-commit': commit_new_record})
-
-
+thread = db_subscriber.run_in_thread(sleep_time=0.001)
 
 @app.route("/api", methods=['GET'])
 def hello():
@@ -61,7 +62,7 @@ def hello():
     source = request.args.get('dataset')
     source = '*' if source is None else str(source)
 
-    response_subscriber = redis_instance.pubsub()
+    response_subscriber = redis_instance.pubsub(ignore_subscribe_messages=True)
     response_subscriber.subscribe('db-records-filter-'+source)
 
     if redis_instance.sadd('set:db-records-filter', source) == 1:
@@ -69,44 +70,61 @@ def hello():
 
     for message in response_subscriber.listen():
         redis_instance.srem('set:db-records-filter', source)
-        status = 200 if message['data'] != '""' else 400 
-        response = app.response_class(data=message['data'], status_code=status, mimetype='application/json')
+        status_code = 200 if message['data'] != '""' else 400 
+        response = app.response_class(response=message['data'], status=status_code, mimetype='application/json')
         break
 
     response_subscriber.close()
     return response
 
 
-@app.route('/api/new', methods=['POST'])
-def add_comment(timecr = time.time(), username='cruyff8', comment=None):
-   
-    if comment is None: 
-        comment = request.args.get('comment')
-    if comment is None:
-        return jsonify({'Status': "'comment' is None"}), 400 
-
-    response_subscriber = redis_instance.pubsub()
+def publish_add_request(username='cruyff8', timecr=None, comment=None):
+    timecr = time.time() if timecr is None else timecr
+    
+    response_subscriber = redis_instance.pubsub(ignore_subscribe_messages=True)
     response_subscriber.subscribe('db-records-commit-'+username+':'+str(timecr))
-    redis_instance.publish('db-records-commit', username+':'+str(createtime)+':'+comment)
+    redis_instance.publish('db-records-commit', username+':'+str(timecr)+':'+comment)
 
     for message in response_subscriber.listen():
-        status = 201 if 'failure' not in message['data'] else 400
-        response = app.response_class(data=message['data'], status_code=status, mimetype='application/json')
+        status_code = 201 if 'failure' not in message['data'] else 400
+        response = app.response_class(response=message['data'], status=status_code, mimetype='application/json')
         break
 
     response_subscriber.close()
     return response 
 
+
+@app.route('/api/new', methods=['POST'])
+def add_comment(username='cruyff8', timecr=None, comment=None):
+
+    timecr = str(time.time()) if timecr is None else timecr
+    comment = request.args.get('comment') if comment is None else comment
+    if comment is None:
+        return jsonify({'Status': "'comment' is None"}), 400 
+    return json.dumps("")
+    # return publish_add_request(username=username, timecr=timecr, comment=comment)
+
+    # response_subscriber = redis_instance.pubsub(ignore_subscribe_messages=True)
+    # response_subscriber.subscribe('db-records-commit-'+username+':'+str(timecr))
+    # redis_instance.publish('db-records-commit', username+':'+str(timecr)+':'+comment)
+
+    # for message in response_subscriber.listen():
+    #     status_code = 201 if 'failure' not in message['data'] else 400
+    #     response = app.response_class(response=message['data'], status=status_code, mimetype='application/json')
+    #     break
+
+    # response_subscriber.close()
+    # return response 
+
 @app.route('/api', methods=['POST'])
 def hello_add(data=None):
-    
     if data is None:
         data = request.args.post('data')
     if data is None or data[0] is None or data[1] is None or data[2] is None:
         return jsonify({'Status': "'data' is None"}), 400
-
-    return add_comment(username=data[0], timecr=data[1], comment=data[2])
-
+    return publish_add_request(username=data[0], timecr=data[1], comment=data[2])
+    # return add_comment(username=data[0], timecr=data[1], comment=data[2])
+    # return json.dumps("")
 
 def get_comments():
     to_db = StringIO.StringIO()
@@ -128,6 +146,11 @@ def get_comments():
     return json.dumps('Imported {0} records'.format(count))
 
 
+def close_handlers(signum, frame):
+    thread.stop()
+    db_subscriber.close()
+    sys.exit(0)
+
 @app.route('/', methods=['GET'])
 def root():
     return send_from_directory('static', 'index.html')
@@ -135,16 +158,15 @@ def root():
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
+    signal.signal(signal.SIGINT, close_handlers)     
+
     # Recreate tables
     db.drop_all()
     db.create_all()
     db.session.commit()
 
     app.config.update(TESTING=True, DEBUG=True, JSONIFY_PRETTYPRINT_REGULAR=False, SQLALCHEMY_TRACK_MODIFICATIONS=False)
-    app.run(port=5001, debug=True)
 
     get_comments()
+    app.run(port=5001, debug=True)
 
-    for msg in db_subscriber.listen():
-        # for each message we have set up a handler
-        pass
